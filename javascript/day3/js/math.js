cde2f0c7e695766ae81431a01f075edb6e77c4b5
Unknown <ufo154123@naver.com>
2018-06-04 12:31:34 +0900

    javascript 3회차 : 매스 객체 예제

---
+++ javascript/day3/js/math.js
@@ -0,0 +1,23 @@
+console.log("Math.PI : " + Math.PI);
+console.log("Math.E : " + Math.E);
+
+//제곱함수
+console.log("Math.pow : " + Math.pow(2,3));
+
+//제곱근 함수
+console.log("Math.sqrt(4) : " + Math.sqrt(4));
+
+//소수 첫번째 자리에서 올려주는 것~~! 
+console.log("Math.ceil(1.3) :" + Math.ceil(1.3));
+
+//두 수 중 작은 수를 반환하는 것
+console.log("Math.min(1,4) : " + Math.min(1,4));
+//두 수 중 큰 수를 반환하는 것
+console.log("Math.max(1,6) : " + Math.max(1,6));
+
+console.log("Math.random() : " + Math.random());
+
+//반올림
+console.log("Math.round(1.3) : " + Math.round(1.3));
+//내림
+console.log("Math.floor(1.9) :" + Math.floor(1.9) );
