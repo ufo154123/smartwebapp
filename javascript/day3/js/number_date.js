cc4e058ebb7e30183cf3171cdad85cf52ea78b15
Unknown <ufo154123@naver.com>
2018-06-04 12:31:49 +0900

    javascript 3회차 넘버 객체 예제

---
+++ javascript/day3/js/number_date.js
@@ -0,0 +1,4 @@
+console.log("MAX_VALUE : " + Number.MAX_VALUE);
+console.log("MIN_VALUE : " + Number.MIN_VALUE);
+console.log("NEGATIVE VALUE : " + Number.NEGATIVE_INFINITY);
+console.log("POSITIVE VALUE : " + Number.POSITIVE_INFINITY);
